2fcf47cecd03b80461524488914845bb68f5ee4a
sanjana-t <73529434+sanjana-t@users.noreply.github.com>
2024-05-20 22:53:23 +0530

    Update crawler.py

--- api/app/src/routes/crawler.py
+++ api/app/src/routes/crawler.py
@@ -30,29 +30,67 @@ async def result_api():
     else:
         return get_result()
 
-@router.get("/req_links")
-def get_resquest_responses():
+@router.post("/req_links")
+def get_all_requested_links(pagedetails :PaginationItem):
     filename = 'requests_responses.json'
     contents = {}
-    req=[]
     try:
         with open(filename, 'r') as f:
             contents = json.load(f)
     except Exception as e:
         print(e)
+        return []
+
+    urls = set()
     if 'log_list' in contents:
-        urls = set()
+        start_index = (pagedetails.page - 1) * pagedetails.links_per_page
+        end_index = start_index + pagedetails.links_per_page
+        current_index = 0
+
         for item in contents['log_list']:
             if 'request' in item:
                 request_value = item['request']
                 if isinstance(request_value, str):
-                    urls.add(request_value)
+                    if start_index <= current_index < end_index:
+                        urls.append(request_value)
+                    current_index += 1
                 elif isinstance(request_value, list):
                     for request_item in request_value:
-                            urls.add(request_item)
+                        if start_index <= current_index < end_index:
+                            urls.append(request_item)
+                        current_index += 1
+                        if current_index >= end_index:
+                            break
+            if current_index >= end_index:
+                break
 
-        unique_urls = list(urls)
-        return unique_urls
-    else:
-        urls = set()
-        return list(urls)
+    return list(urls)
+
+
+
+# @router.get("/req_links")
+# def get_resquest_responses():
+#     filename = 'requests_responses.json'
+#     contents = {}
+#     req=[]
+#     try:
+#         with open(filename, 'r') as f:
+#             contents = json.load(f)
+#     except Exception as e:
+#         print(e)
+#     if 'log_list' in contents:
+#         urls = set()
+#         for item in contents['log_list']:
+#             if 'request' in item:
+#                 request_value = item['request']
+#                 if isinstance(request_value, str):
+#                     urls.add(request_value)
+#                 elif isinstance(request_value, list):
+#                     for request_item in request_value:
+#                             urls.add(request_item)
+
+#         unique_urls = list(urls)
+#         return unique_urls
+#     else:
+#         urls = set()
+#         return list(urls)
